bab76c3649fadff361ef3b48c5508b72f9156601
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-10-22 22:56:35 -0300

    Add files via upload

---
+++ IdiomaSistema/src/main/java/com/mycompany/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,12 @@
+package com.mycompany.idiomasistema;
+
+import java.util.Locale;
+
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale.getDisplayLanguage());
+        System.out.println(locale.getLanguage());
+    }
+}
